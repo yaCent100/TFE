15b93c5253377a1451a6eadc9e0472d98bfd7014
yassin <iallaleny@gmail.com>
2024-03-03 20:54:38 +0100

    first commit

---
+++ src/main/java/be/iccbxl/tfe/Driveshare/DriveshareApplication.java
@@ -0,0 +1,13 @@
+package be.iccbxl.tfe.Driveshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/iccbxl/tfe/Driveshare/DriveshareApplicationTests.java
@@ -0,0 +1,13 @@
+package be.iccbxl.tfe.Driveshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
