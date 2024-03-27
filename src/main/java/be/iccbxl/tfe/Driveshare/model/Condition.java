6403bb74801d850bd1a1547e89a7d17131f52479
yassin <iallaleny@gmail.com>
2024-03-27 14:52:45 +0100

    ajout du model condition

---
+++ src/main/java/be/iccbxl/tfe/Driveshare/model/Condition.java
@@ -0,0 +1,20 @@
+package be.iccbxl.tfe.Driveshare.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="conditions")
+public class Condition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="description")
+    private String condition;
+
+    @ManyToOne
+    private Cars car;
+}
